b878d86d28d9790ecad697db83af71f59154303b
Ariyan Aftab Spandan <ariyanspandan@gmail.com>
2026-01-14 01:16:46 +0600

    feat: update Bloglist component to enhance button styles and maintain layout structure

--- src/app/component/Bloglist.jsx
+++ src/app/component/Bloglist.jsx
@@ -1,22 +1,28 @@
+import { blog_data } from '@/asset/assets'
+import Blogitem from './Blogitem'
 import React from 'react'
 
 const Bloglist = () => {
   return (
     <div>
       <div className='flex justify-center gap-6 my-10'>
-        <button className='bg-black text-white py-1 px-4 rounded-sm'>
+        <button className='hover:bg-black hover:text-white py-1 px-4 rounded-sm'>
           All
         </button>
-        <button>
+        <button className='hover:bg-black hover:text-white py-1 px-4 rounded-sm'>
           Technology
         </button>
-        <button>
+        <button className='hover:bg-black hover:text-white py-1 px-4 rounded-sm'>
           Startup
         </button>
-        <button>
+        <button className='hover:bg-black hover:text-white py-1 px-4 rounded-sm'>
           Lifestyle
         </button>
-
+      </div>
+      <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24 '>
+        {blog_data.map((item, index) => {
+          return <Blogitem key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
+        })}
       </div>
     </div>
   )
