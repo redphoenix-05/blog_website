181fbd9ff09e54db634f7ae266494919da444d35
Ariyan Aftab Spandan <ariyanspandan@gmail.com>
2026-01-13 19:51:35 +0600

    feat: add Blogitem component to display blog details on the page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
+'use client'
 import React from 'react'
 import Header from './component/Header'
+import Blogitem from './component/Blogitem'
 
 export default function page() {
   return (
     <>
       <Header />
+      <Blogitem />
     </>
   )
 }
